d994a5b207194f98c1ed891cc33b208c1febc80b
Tacio <taciodesouzacampos@gmail.com>
2022-11-05 00:09:52 -0300

    update next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import Link from 'next/link';
 
 import ThemeContext from 'contexts/themeContext';
@@ -14,6 +14,45 @@ import pgpImage from 'public/assets/images/pgp.png';
 import osFederaImage from 'public/assets/images/osfederas.png';
 import covid19Image from 'public/assets/images/covid19.jpg';
 
+const cards = [
+  {
+    title: 'SACA',
+    content: 'Aplicativo móvel voltado para ajudar crianças com problemas de comunicação',
+    link: 'https://play.google.com/store/apps/details?id=xyz.saca',
+    image: sacaImage,
+  },
+  {
+    title: 'SAED',
+    content: 'Sistema de avaliação de desempenho escolar através da resolução de questões elobaradas pelos professores',
+    link: 'https://saed.t4zo.com',
+    image: saedImage,
+  },
+  {
+    title: 'PGP',
+    content: 'Programa de Governo Participativo, um portal feito em 2020 para o então candidato a prefeito de Juazeiro, Paulo Bomfim no período eleitoral',
+    link: 'https://pgpjuazeiroba.com.br',
+    image: pgpImage,
+  },
+  {
+    title: 'Netflix',
+    content: 'Projeto pessoal realizando um clone com a possibilidade de criação de contas e atualização periódica do catálogo de Séries e Filmes',
+    link: 'https://netflix-blush.vercel.app',
+    image: netflixImage,
+  },
+  {
+    title: 'Covid19',
+    content: 'Sistema para rastreamento de casos e mortes confirmadas da Covid19 por país ou globalmente',
+    link: 'https://covid19-tracker-azure.vercel.app',
+    image: covid19Image,
+  },
+  {
+    title: 'Os Federa',
+    content: 'Projeto para o professor, afim de atualizar seu site pessoal',
+    link: 'https://osfedera.netlify.app',
+    image: osFederaImage,
+  },
+];
+
 export default function HomePage() {
   const { darkTheme, toggleTheme } = useContext(ThemeContext);
 
@@ -23,20 +62,17 @@ export default function HomePage() {
         <Switch condition={darkTheme} changeCondition={toggleTheme} offIcon={'&#x2600;&#xFE0F;'} onIcon={'&#127769;'} />
         <h1 className='text-center m-0 text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-tight'>
           <span className='hover:text-primary-600 hover:cursor-pointer dark:text-gray-200 dark:hover:text-primary-600'>
-            <span className='text-primary-600'>T</span>ácio de <span className='text-primary-600'>S</span>ouza <span className='text-primary-600'>C</span>ampos
+            <span className='text-primary-600'>T</span>ácio de <span className='text-primary-600'>S</span>ouza <span className='text-primary-600'>C</span>ampos.
           </span>
           <span> | </span>
-          <Link href='/contact'>
-            <a className='text-primary-600 no-underline hover:underline focus:underline'>Contato</a>
+          <Link href='/contact' className='text-primary-600 no-underline hover:underline focus:underline'>
+            Contato
           </Link>
         </h1>
         <div className='grid gap-5 grid-cols-1 lg:grid-cols-2 w-full mt-12 lg:max-w-6xl'>
-          <Card link='https://play.google.com/store/apps/details?id=xyz.saca' image={sacaImage} title='SACA' content='Aplicativo móvel voltado para ajudar crianças com problemas de comunicação' />
-          <Card link='https://saed.t4zo.com' image={saedImage} title='SAED' content='Sistema de avaliação de desempenho escolar através da resolução de questões elobaradas pelos professores' />
-          <Card link='https://pgpjuazeiroba.com.br' image={pgpImage} title='PGP' content='Programa de Governo Participativo, um portal feito em 2020 para o então candidato a prefeito de Juazeiro, Paulo Bomfim no período eleitoral' />
-          <Card link='https://netflix-blush.vercel.app' image={netflixImage} title='Netflix' content='Projeto pessoal realizando um clone com a possibilidade de criação de contas e atualização periódica do catálogo de Séries e Filmes' />
-          <Card link='https://covid19-tracker-azure.vercel.app' image={covid19Image} title='Covid19' content='Sistema para rastreamento de casos e mortes confirmadas da Covid19 por país ou globalmente' />
-          <Card link='https://osfedera.netlify.app' image={osFederaImage} title='Os Federa' content='Projeto para o professor, afim de atualizar seu site pessoal' />
+          {cards.map(card => (
+            <Card title={card.title} content={card.content} link={card.link} image={card.image} />
+          ))}
         </div>
       </main>
       <Footer />
