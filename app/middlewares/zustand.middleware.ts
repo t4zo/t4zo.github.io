45cf3d0fc6746b781750c7f5122d3047fed3ea65
t4zo <t4zo@users.noreply.github.com>
2024-01-29 21:43:41 +0000

    deploy: t4zo/t4zo.github.io@a954e6bb4efe9352967b266256aa083a33ee9f22

--- app/middlewares/zustand.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { useContactStore } from 'stores/contact.store'
-import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
-
-// Usage with a plain action store, it will log actions as "setState"
-const usePlainStore = create(devtools(useContactStore))
-// Usage with a redux store, it will log full action types
-// const useReduxStore = create(devtools(redux(reducer, initialState)))
