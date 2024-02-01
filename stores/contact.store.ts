c24064d6bb76f8c3d60f4c21e0b1b7e801e28a98
Tacio de Souza Campos <taciodesouzacampos@gmail.com>
2024-02-01 20:12:59 -0300

    clear state

--- stores/contact.store.ts
+++ stores/contact.store.ts
@@ -1,15 +1,9 @@
-import { create } from 'zustand'
 import { Contact } from 'types/contact.type';
+import { create } from 'zustand';
 
 type ContactStoreState = {
   contact: Contact;
-  name: string;
-  email: string;
-  message: string;
   setContact(contact: Contact): void;
-  setName(name: string): void;
-  setEmail(email: string): void;
-  setMessage(message: string): void;
 }
 
 export const useContactStore = create<ContactStoreState>(set => ({
@@ -18,11 +12,5 @@ export const useContactStore = create<ContactStoreState>(set => ({
     email: '',
     message: '',
   },
-  name: '',
-  email: '',
-  message: '',
   setContact: (contact: Contact) => set(_ => ({ contact })),
-  setName: (name: string) => set(_ => ({ name })),
-  setEmail: (email: string) => set(_ => ({ email })),
-  setMessage: (message: string) => set(_ => ({ message })),
 }))
